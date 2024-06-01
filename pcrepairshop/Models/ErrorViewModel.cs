c4ac3e841c358a43f5e5c939326d326a1c4cd976
Gerhard <gerhardjpotgieter@gmail.com>
2024-06-01 13:51:47 +0200

    Deleted standard .Net to create .net core app

--- pcrepairshop/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace pcrepairshop.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
