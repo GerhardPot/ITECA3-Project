a657fc559246d6fdd4d94000c0047ec8205a906e
Gerhard <gerhardjpotgieter@gmail.com>
2024-06-18 21:38:38 +0200

    Initial create Set connection string to database

---
+++ pcrepairshop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pcrepairshop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
