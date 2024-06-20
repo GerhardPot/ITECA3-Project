e085f2a09a7368b059ee6746465b26b5410fc02d
Gerhard <gerhardjpotgieter@gmail.com>
2024-06-20 17:16:16 +0200

    Added Identity Framework

--- PCRepairShop/Migrations/20240620151248_Identity.Designer.cs
+++ PCRepairShop/Migrations/20240620151248_Identity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using pcrepairshop.Data;
 
@@ -11,9 +12,11 @@ using pcrepairshop.Data;
 namespace pcrepairshop.Migrations
 {
     [DbContext(typeof(pcrepairshopDbContext))]
-    partial class pcrepairshopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240620151248_Identity")]
+    partial class Identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- pcrepairshop/Models/User.cs
+++ pcrepairshop/Models/User.cs
@@ -5,7 +5,6 @@ namespace pcrepairshop.Models
 {
     public class User : IdentityUser
     {
-        //[Key] public int Id { get; set; }
         [Required] [MaxLength(40)]
         public string Name { get; set; }
         [Required] [MaxLength(40)] 
