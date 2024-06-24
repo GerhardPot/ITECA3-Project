c3e0b98facd5d4ea9578ca234b3633a81eb8ad73
Gerhard Potgieter <152413553+GerhardPot@users.noreply.github.com>
2024-06-24 11:07:27 +0200

    Delete PCRepairShop directory

--- PCRepairShop/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PCRepairShop.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
